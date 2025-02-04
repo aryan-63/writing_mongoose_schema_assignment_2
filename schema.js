3081d7c4a30123fba7c0e52bba41f4233e59f072
Aryan Raj <adityaraj@Adityas-MacBook-Air.local>
2025-02-04 11:39:44 +0530

    completed

---
+++ schema.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+
+
+
+const BlogPostSchema = new mongoose.Schema({
+
+  title: {
+    type: String,
+    unique: true,
+    minlength: 5,
+  },
+
+
+  content: {
+    type: String,
+    required: true,
+    minlength: 50,
+  },
+
+
+  author: {
+    type: String,
+  },
+
+
+  tags: {
+    type: [String],
+    default: [],
+  },
+
+
+  category: {
+    type: String,
+    default: 'General',
+  },
+
+
+  likes: {
+    type: [String],
+    default: [],
+  },
+
+
+  comments: [CommentSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  }
+  
+});
+
+
+const CommentSchema = new mongoose.Schema({
+
+
+    username: {
+      type: String,
+    },
+
+    message: {
+      type: String,
+      required: true,
+    },
+
+    commentedAt: {
+      type: Date,
+      default: Date.now,
+    }
+
+  });
+
+
+
+module.exports = mongoose.model('BlogPost', BlogPostSchema);
